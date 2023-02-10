7269252738992f2fcf99f209e27a4d01030bc131
/Xenz404 <116076493+Xenz404@users.noreply.github.com>
2023-02-10 12:19:40 +0700

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,11 @@
+#Hai bang mau ngpain
+#Mau recode ya_-
+import os
+if __name__ == "__main__":
+	try:
+		os.system("git pull")
+		__import__("file").Main()
+	except Exception as e:
+		exit(str(e))
+
+
